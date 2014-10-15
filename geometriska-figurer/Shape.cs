cce6ec04ca337702c77261ebaeca2cfd79a1e878
test <test@test.test>
2014-10-15 11:48:59 +0200

    s

--- geometriska-figurer/Shape.cs
+++ geometriska-figurer/Shape.cs
@@ -7,5 +7,58 @@ namespace geometriska_figurer
 {
     public abstract class Shape : IComparable 
     {
+        private double _length;
+        private double _width;
+
+        protected Shape()
+            : this(0, 0)
+        {
+
+        }
+        protected Shape(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+
+        public abstract double Area { get; }
+        public abstract double Perimeter { get; }
+
+
+        public double Length
+        {
+            get
+            {
+               
+            }
+            set
+            {
+            
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                
+            }
+            set
+            {
+               
+            }
+        }
+
+
+        public int CompareTo(Object obj)
+        {
+
+        }
+
+        public override string ToString()
+        {
+
+        }
+
     }
 }

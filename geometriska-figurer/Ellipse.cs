de13d2142449528f5524b4eb82cabbfd9d071b4b
test <test@test.test>
2014-10-15 11:39:58 +0200

    Klasserna klara

---
+++ geometriska-figurer/Ellipse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace geometriska_figurer
+{
+    public class Ellipse : Shape
+    {
+    }
+}
---
+++ geometriska-figurer/Shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace geometriska_figurer
+{
+    public abstract class Shape : IComparable 
+    {
+    }
+}
